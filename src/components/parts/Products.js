2e7dc8db9af1f7fa1577e6c22977aa4670d01835
nownabe <nownabe@gmail.com>
2016-09-23 03:12:12 +0900

    :flower_playing_cards: Fix description

--- src/components/parts/Products.js
+++ src/components/parts/Products.js
@@ -7,7 +7,7 @@ export default () => {
       url: "https://x86-emulator.nownabe.com/",
       title: "x86-emulator",
       description:
-        "Emulator of x86 CPU to learn about x86 assembly, created with React/Redux. \
+        "An emulator of x86 CPU to learn about x86 assembly, created with React/Redux. \
         This emulator can execute i386 assembly programs and show data in registers and memory step by step. \
         It allows you to learn x86 assembly."
     },
