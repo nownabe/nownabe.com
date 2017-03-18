a7f9db57a77fb3eab066ff9a0d4ca06073f558f3
nownabe <nownabe@gmail.com>
2017-03-18 23:23:39 +0900

    :tada: Add introduction

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,11 +7,18 @@ import styles from './styles.css';
 
 export default () => (
   <header className={styles.header}>
+    <h1 className={styles.title}>
+      <Link to="/" className={styles.link}>nownabe.github.io</Link>
+    </h1>
     <div className={styles.titleContainer}>
       <Icon />
-      <h1 className={styles.title}>
-        <Link to="/" className={styles.link}>nownabe</Link>
-      </h1>
+      <div className={styles.introduction}>
+        <p>
+          Hello. I'm Shogo Watanabe, a software engineer from Japan.
+          I like coding, traveling, reading comics, playing drums, playing online games.
+          Now I'm working on web development.
+        </p>
+      </div>
     </div>
   </header>
 );
