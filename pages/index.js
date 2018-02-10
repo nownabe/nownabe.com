47234e25b82c1bedc2e63bdc1381f87434b95ca2
nownabe <nownabe@gmail.com>
2018-02-10 17:40:27 +0900

    :fire: Kill Next.js

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Layout from '../components/common/Layout';
-import Home from '../components/Home';
-
-export default () => <Layout><Home /></Layout>;
--- pages/rubygems.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-import Layout from '../components/common/Layout';
-import Rubygems from '../components/Rubygems';
-
-export default () => <Layout><Rubygems /></Layout>;
